45eae66136e40bad681275dde6410c5a60afe8e2
Neil Rhodes <neil@pobox.com>
2022-02-04 11:17:49 -0800

    Complete step 2 of React components exercise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+const data = [
+  {
+    id: 512,
+    name: "Neil Rhodes",
+    email: "rhodes@hmc.edu",
+    phone: "(909) 555-1212"
+  },
+  {
+    id: 787,
+    name: "Barack Obama",
+    email: "ex-prez@whitehouse.gov",
+    phone: "(312) 555-1212"
+  }
+];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
