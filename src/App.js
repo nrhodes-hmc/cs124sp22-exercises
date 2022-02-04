ca0b498d5e889c45f6328cc4a0cfd7265f92c255
Neil Rhodes <neil@pobox.com>
2022-02-04 11:32:15 -0800

    Complete step 3 of React components exercise

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
 const data = [
-  {
-    id: 512,
-    name: "Neil Rhodes",
-    email: "rhodes@hmc.edu",
-    phone: "(909) 555-1212"
-  },
-  {
-    id: 787,
-    name: "Barack Obama",
-    email: "ex-prez@whitehouse.gov",
-    phone: "(312) 555-1212"
-  }
+    {
+        id: 512,
+        name: "Neil Rhodes",
+        email: "rhodes@hmc.edu",
+        phone: "(909) 555-1212"
+    },
+    {
+        id: 787,
+        name: "Barack Obama",
+        email: "ex-prez@whitehouse.gov",
+        phone: "(312) 555-1212"
+    }
 ];
 
+function Person(props) {
+    return <tr>
+        <td className={"name"}>{props.name}</td>
+        <td className={"email"}>{props.email}</td>
+        <td className={"phone"}>{props.phone}</td>
+    </tr>;
+}
+
+function People(props) {
+    return <table>
+        <tbody>
+        {props.people.map(p => <Person {...p}/>)}
+        </tbody>
+    </table>
+}
+
 function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+    /*
+    <Person {...p}/>
+     is equivalent to:
+     <Person name={p.name} email={p.email} phone={p.phone}/>
+
+     <>
+     is equivalent to:
+     <React.Fragment>
+     */
+    return <>
+        <h1>People</h1>
+        <People people={data}/>
+    </>;
 }
 
 export default App;
