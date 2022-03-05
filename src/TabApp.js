89857cedba8d32d47660ff8be949648166fa2107
Neil Rhodes <neil@pobox.com>
2022-03-04 18:43:18 -0800

    Add TabList and such

---
+++ src/TabApp.js
@@ -0,0 +1,12 @@
+import TabList from "./TabList";
+
+function TabApp() {
+    return (
+        <TabList>
+            <div key="Tab 1">Hello, world!</div>
+            <div key="Tab 2">Goodbye, universe!</div>
+        </TabList>
+    );
+}
+
+export default TabApp;
