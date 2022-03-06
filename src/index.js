6eb8456b1f95397ce78b50561fcddf89b90d4cf6
Neil Rhodes <neil@pobox.com>
2022-03-05 18:20:19 -0800

    Step 5: Create INITIAL_STATE, rootReducer, store, and Provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+
+const INITIAL_STATE = {
+    data: [
+        {
+            id: 512,
+            name: "Neil Rhodes",
+            email: "rhodes@hmc.edu",
+            phone: "(909) 555-1212"
+        },
+        {
+            id: 787,
+            name: "Barack Obama",
+            email: "ex-prez@whitehouse.gov",
+            phone: "(312) 555-1212"
+        }
+    ],
+    selectedId: null
+}
+
+function rootReducer(state = INITIAL_STATE,
+                     action) {
+    return state;
+}
+
+const store = createStore(rootReducer, INITIAL_STATE);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
